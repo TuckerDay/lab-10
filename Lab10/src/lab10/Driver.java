61c314bb6956f807c130b5cd972d36e8d356f9fa
TuckerDay <37190942+TuckerDay@users.noreply.github.com>
2018-04-27 15:27:30 -0600

    Add files via upload
    Program scans files and creates arrays of every word.

---
+++ Lab10/src/lab10/Driver.java
@@ -0,0 +1,33 @@
+package lab10;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class Driver {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		// File reading variables
+		File bookFile1 = new File("./src/lab10/Coyote.txt");
+		File bookFile2 = new File("./src/lab10/Coyote and the Bitterroot Valley.txt");
+		
+		ArrayList<String> bookArray1, bookArray2;
+		
+		
+		// Read book 1
+		Reader reader1 = new Reader(bookFile1);
+		bookArray1 = new ArrayList<String>(reader1.getArray());
+		
+		// Read book 2
+		Reader reader2 = new Reader(bookFile2);
+		bookArray2 = new ArrayList<String>(reader2.getArray());
+			/*for (int i = 0; i < bookArray2.size(); i++) {
+				System.out.println(bookArray2.get(i));*/
+		
+		
+		
+		
+	}
+
+}
